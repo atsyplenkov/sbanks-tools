537787e79005c744a8c09ccd39981b2252c9c290
atsyplenkov <sharp.couch9400@fastmail.com>
2026-01-18 23:38:02 +0300

    fix qgis tests

---
+++ qgis/tests/conftest.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Pytest configuration for QGIS Sbanks plugin tests.
+
+Sets up sys.path to include vendor dependencies before tests run.
+"""
+
+import os
+import sys
+
+# Add vendor/sbanks-lib to sys.path for sbanks_core imports
+_tests_dir = os.path.dirname(os.path.abspath(__file__))
+_plugin_dir = os.path.dirname(_tests_dir)
+_vendor_dir = os.path.join(_plugin_dir, 'sbanks', 'vendor', 'sbanks-lib')
+
+if _vendor_dir not in sys.path:
+    sys.path.insert(0, _vendor_dir)
